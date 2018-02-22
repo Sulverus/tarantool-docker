4c4ded40bfbbe7c2848db1557e58f1bb9a330ec8
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2018-02-22 17:12:37 +0300

    Add 1.x referencing 1.9.0

---
+++ 1.x/luarocks-config.lua
@@ -0,0 +1,9 @@
+rocks_trees = {
+   { name = [[user]], root = home..[[/.luarocks]] },
+   { name = [[system]], root = [[/usr/local]] }
+}
+
+rocks_servers = {
+    [[http://rocks.tarantool.org/]],
+    [[http://luarocks.org/repositories/rocks]]
+}
