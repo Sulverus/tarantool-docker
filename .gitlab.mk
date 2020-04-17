6f860e6e2b027c56de092dd958490598f927eede
Alexander V. Tikhonov <avtikhon@tarantool.org>
2020-04-17 12:04:22 +0300

    Disabled docker cache for images build
    Found that some docker layers must be rebuilt,
    to make it able the docker caching disabled.

--- .gitlab.mk
+++ .gitlab.mk
@@ -1,7 +1,7 @@
 TNT_VER=$(shell cat versions/${OS}_${VER})
 
 build:
-	docker build --network=host --build-arg TNT_VER=${TNT_VER} \
+	docker build --no-cache --network=host --build-arg TNT_VER=${TNT_VER} \
 		-t ${IMAGE}:${TAG} -f dockerfiles/${OS}_${DVER} .
 	docker run --rm --name tarantool_${TAG} -p ${PORT}:${PORT} -d ${IMAGE}:${TAG}
 	docker exec -t tarantool_${TAG} tarantool_is_up
