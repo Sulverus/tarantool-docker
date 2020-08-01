5befbb18f9f43c28f2a4219909ea4227e46a5ca6
Alexander V. Tikhonov <avtikhon@tarantool.org>
2020-08-01 08:50:36 +0300

    Set rocks tool installer using variable
    Added ROCKS_INSTALLER variable for dockerfile build to be able
    to set needed rocks tool installer.

--- .gitlab.mk
+++ .gitlab.mk
@@ -1,7 +1,10 @@
 TNT_VER=$(shell cat versions/${OS}_${DIST}_${VER})
+ROCKS_INSTALLER?='tarantoolctl rocks'
 
 build:
-	docker build --no-cache --network=host --build-arg TNT_VER=${TNT_VER} \
+	docker build --no-cache --network=host \
+		--build-arg ROCKS_INSTALLER=${ROCKS_INSTALLER} \
+		--build-arg TNT_VER=${TNT_VER} \
 		--build-arg BASE_IMAGE="${OS}:${DIST}" \
 		-t ${IMAGE}:${TAG} -f dockerfiles/${OS}_${DIST}_${DOCKERFILE_NAME_SUFFIX} .
 	docker run --rm --name tarantool_${TAG} -p ${PORT}:${PORT} -d ${IMAGE}:${TAG}
