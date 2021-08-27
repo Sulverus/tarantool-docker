632d7e8cec200b1a27c33f8555239fdf928f6c7c
VitaliyaIoffe <vitaioffe@yandex.ru>
2021-08-27 21:11:19 +0300

    Enable build different architectures
    Since 2.10.0-beta1 release tarantool supports ARM architecture.
    Support it in the official docker images.
    
    Build command example:
    'export TAG=2.10.0-beta1-amd64 && export OS=alpine DIST=3.9 VER=2.10.0-beta1 ARCH=amd64/ && PORT=5200 make -f .gitlab.mk build'
    
    Close tarantool/tarantool#5868
    Close tarantool/tarantool#6154

--- .gitlab.mk
+++ .gitlab.mk
@@ -1,18 +1,19 @@
 TNT_VER=$(shell cat versions/${OS}_${DIST}_${VER})
 ROCKS_INSTALLER?='tarantoolctl rocks'
 ENABLE_BUNDLED_LIBYAML?='ON'
-IMAGE?='tarantool/tarantool'
+IMAGE?=tarantool/tarantool
+NPROC?=4
 
 build:
 	[ "${TNT_VER}" != "" ] || (echo "ERROR: TNT_VER not defined" ; exit 1)
-	[ "${DOCKERFILE_NAME_SUFFIX}" != "" ] && \
-		DOCKERFILE_SUFFIX=_${DOCKERFILE_NAME_SUFFIX} ; \
 	docker build --no-cache --network=host \
 		--build-arg ROCKS_INSTALLER=${ROCKS_INSTALLER} \
 		--build-arg ENABLE_BUNDLED_LIBYAML=${ENABLE_BUNDLED_LIBYAML} \
 		--build-arg TNT_VER=${TNT_VER} \
-		--build-arg BASE_IMAGE="${OS}:${DIST}" \
-		-t ${IMAGE}:${TAG} -f dockerfiles/${OS}_${DIST}$${DOCKERFILE_SUFFIX} .
+		--build-arg NPROC=${NPROC} \
+		--build-arg BASE_IMAGE="${ARCH}${OS}:${DIST}" \
+		--progress=plain \
+		-t ${IMAGE}:${TAG} -f dockerfiles/${OS}_${DIST} .
 	docker run --rm --name tarantool_${TAG} -p ${PORT}:${PORT} -d ${IMAGE}:${TAG}
 	docker exec -t tarantool_${TAG} tarantool_is_up
 	docker stop tarantool_${TAG}
